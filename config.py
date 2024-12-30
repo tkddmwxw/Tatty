344dbb38ecb6e1fe6b869dff76a1f96c7adad350
Mr ritik <landxyz1809@gmail.com>
2024-12-31 00:32:48 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7619017682:AAG7NI4SCAlSS9s6D7j5kuQR33oj9tdc300")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@RitikXyz099")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6437994839")
+
+
+
+
+
+
+
+  
